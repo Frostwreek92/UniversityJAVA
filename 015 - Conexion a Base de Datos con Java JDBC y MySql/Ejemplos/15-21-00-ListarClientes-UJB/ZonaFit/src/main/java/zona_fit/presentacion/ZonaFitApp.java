797501785c1ce8ef3529972e34d56319f6031271
Frostwreek92 <frostwreek@gmail.com>
2026-04-08 18:12:57 +0200

    Curso universidad JAVA

---
+++ 015 - Conexion a Base de Datos con Java JDBC y MySql/Ejemplos/15-21-00-ListarClientes-UJB/ZonaFit/src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -0,0 +1,54 @@
+package zona_fit.presentacion;
+
+import zona_fit.datos.ClienteDAO;
+import zona_fit.datos.IClienteDAO;
+
+import java.util.Scanner;
+
+public class ZonaFitApp {
+    public static void main(String[] args) {
+        zonaFitApp();
+    }
+
+    private static void zonaFitApp(){
+        var salir = false;
+        var consola = new Scanner(System.in);
+        // Creamos un objeto de la clase clienteDao
+        IClienteDAO clienteDao = new ClienteDAO();
+        while(!salir){
+            try{
+                var opcion = mostrarMenu(consola);
+                salir = ejecutarOpciones(consola, opcion, clienteDao);
+            }catch (Exception e){
+                System.out.println("Error al ejecutar opciones: " + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+
+    private static int mostrarMenu(Scanner consola){
+        System.out.print("""
+                *** Zona Fit (GYM)
+                1. Listar Clientes
+                2. Buscar Cliente
+                3. Agregar Cliente
+                4. Modificar Cliente
+                5. Eliminar Cliente
+                6. Salir
+                Elije una opcion:\s""");
+        return Integer.parseInt(consola.nextLine());
+    }
+
+    private static boolean ejecutarOpciones(Scanner consola, int opcion,
+                                            IClienteDAO clienteDAO){
+        var salir = false;
+        switch (opcion){
+            case 1 -> { // 1. Listar clientes
+                System.out.println("--- Listado de Clientes ---");
+                var clientes = clienteDAO.listarClientes();
+                clientes.forEach(System.out::println);
+            }
+        }
+        return salir;
+    }
+}
---
+++ 015 - Conexion a Base de Datos con Java JDBC y MySql/Ejemplos/15-23-00-Agregar Cliente-UJB/ZonaFit/src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -0,0 +1,82 @@
+package zona_fit.presentacion;
+
+import zona_fit.datos.ClienteDAO;
+import zona_fit.datos.IClienteDAO;
+import zona_fit.dominio.Cliente;
+
+import java.util.Scanner;
+
+public class ZonaFitApp {
+    public static void main(String[] args) {
+        zonaFitApp();
+    }
+
+    private static void zonaFitApp(){
+        var salir = false;
+        var consola = new Scanner(System.in);
+        // Creamos un objeto de la clase clienteDao
+        IClienteDAO clienteDao = new ClienteDAO();
+        while(!salir){
+            try{
+                var opcion = mostrarMenu(consola);
+                salir = ejecutarOpciones(consola, opcion, clienteDao);
+            }catch (Exception e){
+                System.out.println("Error al ejecutar opciones: " + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+
+    private static int mostrarMenu(Scanner consola){
+        System.out.print("""
+                *** Zona Fit (GYM)
+                1. Listar Clientes
+                2. Buscar Cliente
+                3. Agregar Cliente
+                4. Modificar Cliente
+                5. Eliminar Cliente
+                6. Salir
+                Elije una opcion:\s""");
+        return Integer.parseInt(consola.nextLine());
+    }
+
+    private static boolean ejecutarOpciones(Scanner consola, int opcion,
+                                            IClienteDAO clienteDAO){
+        var salir = false;
+        switch (opcion){
+            case 1 -> { // 1. Listar clientes
+                System.out.println("--- Listado de Clientes ---");
+                var clientes = clienteDAO.listarClientes();
+                clientes.forEach(System.out::println);
+            }
+            case 2 ->{ // 2. Buscar cliente por id
+                System.out.print("Introduce el id del Cliente a buscar: ");
+                var idCliente = Integer.parseInt(consola.nextLine());
+                var cliente = new Cliente(idCliente);
+                var encontrado = clienteDAO.buscarClientePorId(cliente);
+                if(encontrado)
+                    System.out.println("Cliente encontrado: " + cliente);
+                else
+                    System.out.println("Cliente NO encontrado: " + cliente);
+
+            }
+            case 3 -> { // 3. Agregar cliente
+                System.out.println("--- Agregar Cliente ---");
+                System.out.print("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.print("Membresia: ");
+                var membresia = Integer.parseInt(consola.nextLine());
+                // Creamos el objeto cliente (sin el id)
+                var cliente = new Cliente(nombre, apellido, membresia);
+                var agregado = clienteDAO.agregarCliente(cliente);
+                if(agregado)
+                    System.out.println("Cliente agregado: " + cliente);
+                else
+                    System.out.println("Cliente NO agregado: " + cliente);
+            }
+        }
+        return salir;
+    }
+}
