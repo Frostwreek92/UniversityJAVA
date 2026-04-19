571bfe0cb31e57c3d4751050a939abeaca85e46e
Frostwreek92 <frostwreek@gmail.com>
2026-04-19 19:29:49 +0200

    Seccion 15

--- 015 - Conexion a Base de Datos con Java JDBC y MySql/ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
+++ 015 - Conexion a Base de Datos con Java JDBC y MySql/ZonaFit/src/main/java/zona_fit/datos/ClienteDAO.java
@@ -74,16 +74,77 @@ public class ClienteDAO implements IClienteDAO {
 
     @Override
     public boolean agregarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "INSERT INTO cliente(nombre, apellido, membresia)"
+                + " " + "VALUES(?, ?, ?)";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getApellido());
+            ps.setInt(3, cliente.getMembresia());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al agregar cliente: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
         return false;
     }
 
     @Override
     public boolean modificarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "UPDATE cliente SET nombre=?, apellido=?, membresia=?"
+                + " " + "WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getApellido());
+            ps.setInt(3, cliente.getMembresia());
+            ps.setInt(4, cliente.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al modificar cliente: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
         return false;
     }
 
     @Override
     public boolean eliminarCliente(Cliente cliente) {
+        PreparedStatement ps;
+        Connection con = getConexion();
+        String sql = "DELETE FROM cliente WHERE id = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cliente.getId());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Error al eliminar cliente: " + e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            } catch (Exception e) {
+                System.out.println("Error al cerrar conexion: " + e.getMessage());
+            }
+        }
         return false;
     }
 
@@ -95,8 +156,10 @@ public class ClienteDAO implements IClienteDAO {
         IClienteDAO clienteDao = new ClienteDAO();
         var clientes = clienteDao.listarClientes();
         clientes.forEach(System.out::println);
+        System.out.println(); // Salto de linea vacio
 
         // Buscar por ID
+        System.out.println("*** Buscar cliente por ID ***");
         var cliente1 = new Cliente(2);
         System.out.println("Cliente antes de la busqueda: " + cliente1);
         var encontrado = clienteDao.buscarClientePorId(cliente1);
@@ -104,6 +167,39 @@ public class ClienteDAO implements IClienteDAO {
             System.out.println("Cliente encontrado: " + cliente1);
         } else {
             System.out.println("No se encontro registro: " + cliente1.getId());
+        }
+        System.out.println();
+
+        // Agregar cliente
+        System.out.println("*** Agregar cliente ***");
+        var nuevoCliente = new Cliente("Daniel", "Ortiz", 300);
+                var agregado = clienteDao.agregarCliente(nuevoCliente);
+        if (agregado) {
+            System.out.println("Cliente agregado: " + nuevoCliente);
+        } else {
+            System.out.println("No se agrego el cliente: " + nuevoCliente);
+        }
+        System.out.println();
+
+        // Modificar cliente
+        System.out.println("*** Modificar cliente ***");
+        var modificarCliente = new Cliente(4, "Carlos Daniel", "Ortiz", 300);
+        var modificado = clienteDao.modificarCliente(modificarCliente);
+        if (modificado) {
+            System.out.println("Cliente modificado: " + modificarCliente);
+        } else {
+            System.out.println("No se modifico el cliente: " + modificarCliente);
+        }
+        System.out.println();
+
+        // Eliminar cliente
+        System.out.println("*** Eliminar cliente ***");
+        var clienteEliminar = new Cliente(4);
+        var eliminado = clienteDao.eliminarCliente(clienteEliminar);
+        if (eliminado) {
+            System.out.println("Cliente eliminado: " + clienteEliminar);
+        } else {
+            System.out.println("No se elimino cliente: " + clienteEliminar);
         }
     }
 }
